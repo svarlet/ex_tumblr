9bd8086fc7214714bba1a9d9d042bffb75c488e8
Sebastien Varlet <sebastien.varlet@gmail.com>
2016-04-25 23:01:37 +0100

    Define a type/struct for the blog info
    Properties defined by the Tumblr API v2 for the
    blog info call response.

--- lib/ex_tumblr/blog.ex
+++ lib/ex_tumblr/blog.ex
@@ -1,4 +1,34 @@
 defmodule ExTumblr.Blog do
+  alias ExTumblr.Blog
+
+  @typedoc """
+  Represents the properties of a call to the Blog info endpoint.
+
+  More info at https://www.tumblr.com/docs/en/api/v2#blog-info
+  """
+  @type t :: %Blog{
+    title: String.t,
+    posts: non_neg_integer,
+    name: String.t,
+    updated: non_neg_integer,
+    description: String.t,
+    ask: boolean,
+    ask_anon: boolean,
+    likes: non_neg_integer,
+    is_blocked_from_primary: boolean
+  }
+
+  defstruct title: nil,
+    posts: 0,
+    name: nil,
+    updated: 0,
+    description: nil,
+    ask: false,
+    ask_anon: false,
+    likes: 0,
+    is_blocked_from_primary: false
+
+
   @doc """
   Requests general information about the specified blog such as the title,
   number of posts and other high-level data.
