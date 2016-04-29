896fd609086c43304e6ceeb3f4ddbd752db8eaea
Sébastien Varlet <sebastien.varlet@gmail.com>
2016-04-29 17:37:08 +0100

    Delete HTTPoison.Base overriden functions

--- lib/ex_tumblr.ex
+++ lib/ex_tumblr.ex
@@ -1,13 +1,2 @@
 defmodule ExTumblr do
-  use HTTPoison.Base
-
-  @endpoint "https://api.tumblr.com/v2"
-
-  defp process_url(url) do
-    @endpoint <> url
-  end
-
-  defp process_response_body(body) do
-    Poison.decode!(body)
-  end
 end
