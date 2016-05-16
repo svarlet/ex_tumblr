89c50714736254d374e653c87b0fc1e19742f487
Sebastien Varlet <sebastien.varlet@gmail.com>
2016-05-16 22:49:24 +0100

    Write documentation for the new URIExtra module

--- lib/ex_tumblr/utils/uri_extras.ex
+++ lib/ex_tumblr/utils/uri_extras.ex
@@ -1,4 +1,15 @@
 defmodule ExTumblr.Utils.URIExtras do
+  @moduledoc """
+  This module provides extra functions to manipulate URIs.
+  """
+
+  @doc """
+  Builds the path of a URL from a list of path elements.
+
+  This function prefixes each path element with a slash character and concatenates
+  them. If the list of path elements is nil or empty, an empty string is returned.
+  """
+  @spec build_path([String.t]) :: String.t
   def build_path(path_elements) when path_elements in [[], nil] do
     ""
   end
