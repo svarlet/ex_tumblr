9ab73edb3a2fd94eeb9f6e4e9ad3adacd2c6f3c8
Sebastien Varlet <sebastien.varlet@gmail.com>
2016-05-22 13:59:54 +0100

    Fix test warnings

--- lib/ex_tumblr.ex
+++ lib/ex_tumblr.ex
@@ -2,27 +2,4 @@ defmodule ExTumblr do
   defmodule Credentials do
     defstruct consumer_key: nil, consumer_secret: nil, token: nil, token_secret: nil
   end
-
-  defmodule User do
-    @hostname Application.get_env :ex_tumblr, :hostname
-
-    def info(credentials, params) do
-      {:get, "#{@hostname}/v2/user/info", params, :oauth, credentials}
-    end
-
-    def dashboard(credentials, params) do
-      {:get, "#{@hostname}/v2/user/dashboard", params, :oauth, credentials}
-    end
-
-    def likes(credentials, params) do
-      {:get, "#{@hostname}/v2/user/likes", params, :oauth, credentials}
-    end
-
-    def following(credentials, params) do
-      {:get, "#{@hostname}/v2/user/following", params, :oauth, credentials}
-    end
-  end
-
-  def request({method, uri, params, auth_type, credentials}) do
-  end
 end
--- test/ex_tumblr/blog_test.exs
+++ test/ex_tumblr/blog_test.exs
@@ -8,51 +8,51 @@ defmodule ExTumblr.BlogTest do
   end
 
   test "info/3 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/info", params, :api_key_auth, credentials} = Blog.info("gunkatana.tumblr.com", credentials, params)
+    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/info", ^params, :api_key_auth, ^credentials} = Blog.info("gunkatana.tumblr.com", credentials, params)
   end
 
-  test "avatar/2 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/avatar", params, :no_auth, nil} = Blog.avatar("gunkatana.tumblr.com", params)
+  test "avatar/2 creates a valid request", %{credentials: _credentials, params: params} do
+    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/avatar", ^params, :no_auth, nil} = Blog.avatar("gunkatana.tumblr.com", params)
   end
 
   test "followers/3 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/followers", params, :oauth, credentials} = Blog.followers("gunkatana.tumblr.com", credentials, params)
+    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/followers", ^params, :oauth, ^credentials} = Blog.followers("gunkatana.tumblr.com", credentials, params)
   end
 
   test "likes/3 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/likes", params, :api_key_auth, credentials} = Blog.likes("gunkatana.tumblr.com", credentials, params)
+    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/likes", ^params, :api_key_auth, ^credentials} = Blog.likes("gunkatana.tumblr.com", credentials, params)
   end
 
   test "posts/3 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/posts", params, :api_key_auth, credentials} = Blog.posts("gunkatana.tumblr.com", credentials, params)
+    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/posts", ^params, :api_key_auth, ^credentials} = Blog.posts("gunkatana.tumblr.com", credentials, params)
   end
 
   test "queued_posts/3 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/posts/queue", params, :oauth, credentials} = Blog.queued_posts("gunkatana.tumblr.com", credentials, params)
+    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/posts/queue", ^params, :oauth, ^credentials} = Blog.queued_posts("gunkatana.tumblr.com", credentials, params)
   end
 
   test "drafts/3 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/posts/drafts", params, :oauth, credentials} = Blog.drafts("gunkatana.tumblr.com", credentials, params)
+    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/posts/drafts", ^params, :oauth, ^credentials} = Blog.drafts("gunkatana.tumblr.com", credentials, params)
   end
 
   test "submission/3 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/posts/submission", params, :oauth, credentials} = Blog.submission("gunkatana.tumblr.com", credentials, params)
+    assert {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/posts/submission", ^params, :oauth, ^credentials} = Blog.submission("gunkatana.tumblr.com", credentials, params)
   end
 
   test "create/3 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:post, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/post", params, :oauth, credentials} = Blog.create("gunkatana.tumblr.com", credentials, params)
+    assert {:post, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/post", ^params, :oauth, ^credentials} = Blog.create("gunkatana.tumblr.com", credentials, params)
   end
 
   test "edit/3 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:post, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/post/edit", params, :oauth, credentials} = Blog.edit("gunkatana.tumblr.com", credentials, params)
+    assert {:post, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/post/edit", ^params, :oauth, ^credentials} = Blog.edit("gunkatana.tumblr.com", credentials, params)
   end
 
   test "reblog/3 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:post, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/post/reblog", params, :oauth, credentials} = Blog.reblog("gunkatana.tumblr.com", credentials, params)
+    assert {:post, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/post/reblog", ^params, :oauth, ^credentials} = Blog.reblog("gunkatana.tumblr.com", credentials, params)
   end
 
   test "delete/3 creates a valid request", %{credentials: credentials, params: params} do
-    assert {:post, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/post/delete", params, :oauth, credentials} = Blog.delete("gunkatana.tumblr.com", credentials, params)
+    assert {:post, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/post/delete", ^params, :oauth, ^credentials} = Blog.delete("gunkatana.tumblr.com", credentials, params)
   end
 
 
