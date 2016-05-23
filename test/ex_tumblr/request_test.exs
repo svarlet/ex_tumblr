046c383d85d7929c9bb613b368ef55a2dd8e202f
Sebastien Varlet <sebastien.varlet@gmail.com>
2016-05-23 23:51:31 +0100

    Start testing the auth decoration of reqs

---
+++ test/ex_tumblr/request_test.exs
@@ -0,0 +1,9 @@
+defmodule ExTumblr.RequestTest do
+  use ExUnit.Case, async: true
+
+  alias ExTumblr.Request
+
+  test "WIP/Todo: write these tests" do
+    flunk "test not implemented yet"
+  end
+end
