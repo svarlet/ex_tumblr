1394cac4015149e03a1fb152b63478ef0be8d242
Sébastien Varlet <sebastien.varlet@gmail.com>
2016-05-26 11:55:55 +0100

    Writing tests with shouldi

--- test/ex_tumblr/request_test.exs
+++ test/ex_tumblr/request_test.exs
@@ -3,11 +3,21 @@ defmodule ExTumblr.RequestTest do
 
   alias ExTumblr.Request
 
-  should "fail with a not implemented yet error message" do
-    flunk "not implemented yet"
-  end
+  having "No authentication required by the API" do
+    setup context do
+       context
+       |> assign(base_request: {:method, "my_url", :no_auth})
+       |> assign(params: %{my_param: "my_value", my_other_param: "my_other_value"})
+    end
 
-  test "WIP/Todo: write these tests" do
-    flunk "test not implemented yet"
+    should "prepare a request with a nil body", context do
+      {_, _, body, _} = Request.prepare_request_auth(context.base_request, nil, context.params)
+      assert body == nil
+    end
+
+    should "prepare a request with nil headers", context do
+      {_, _, _, headers} = Request.prepare_request_auth(context.base_request, nil, context.params)
+      assert headers == nil
+    end
   end
 end
