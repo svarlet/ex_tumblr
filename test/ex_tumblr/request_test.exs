99f422c36f0a70a1f4c0efddc2c242025f93975d
Sébastien Varlet <sebastien.varlet@gmail.com>
2016-05-26 14:27:22 +0100

    Test the format and encoding of no_auth request params

--- test/ex_tumblr/request_test.exs
+++ test/ex_tumblr/request_test.exs
@@ -7,17 +7,18 @@ defmodule ExTumblr.RequestTest do
     setup context do
        context
        |> assign(base_request: {:method, "my_url", :no_auth})
-       |> assign(params: %{my_param: "my_value", my_other_param: "my_other_value"})
+       |> assign(params: %{my_param: "my value", my_other_param: "my other value"})
     end
 
-    should "prepare a request with a nil body", context do
-      {_, _, body, _} = Request.prepare_request_auth(context.base_request, nil, context.params)
+    should "prepare a request with a no headers and no body", context do
+      {_, _, body, headers} = Request.prepare_request_auth(context.base_request, nil, context.params)
       assert body == nil
+      assert headers == nil
     end
 
-    should "prepare a request with nil headers", context do
-      {_, _, _, headers} = Request.prepare_request_auth(context.base_request, nil, context.params)
-      assert headers == nil
+    should "encode and format the parameters", context do
+      {_, url, _, _} = Request.prepare_request_auth(context.base_request, nil, context.params)
+      assert url == "my_url?my_other_param=my+other+value&my_param=my+value"
     end
   end
 end
