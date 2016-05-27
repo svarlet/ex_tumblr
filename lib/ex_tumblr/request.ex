7524d353f06f24f5293ed4c9a41a1c72385783a4
Sébastien Varlet <sebastien.varlet@gmail.com>
2016-05-27 16:59:32 +0100

    Test the request signing functions

--- lib/ex_tumblr/request.ex
+++ lib/ex_tumblr/request.ex
@@ -1,6 +1,8 @@
 defmodule ExTumblr.Request do
   defstruct [:method, :url, :headers, :body]
 
+  alias ExTumblr.Credentials
+
   @type authorization_headers :: {String.t, String.t}
 
   @type method :: :get | :post
@@ -10,25 +12,28 @@ defmodule ExTumblr.Request do
   @type prepared_request :: {method, url, body, headers}
 
   def prepare_request_auth({method, url, :oauth}, credentials, params) do
-    {headers, body} = Request.sign_request_with_oauth(method, url, params, credentials)
+    {headers, body} = sign_request_with_oauth(method, url, credentials, params)
     {method, url, {:form, body}, [headers]}
   end
 
-  def prepare_request_auth({method, url, :api_key_auth}, credentials, params) do
+  def prepare_request_auth({:get, url, :api_key_auth}, credentials, params) do
     query =
       params
       |> if_nil(Map.new)
       |> Map.put("api_key", credentials.consumer_key)
       |> URI.encode_query
-    {method, "#{url}?#{query}", nil, nil}
+    {:get, "#{url}?#{query}", nil, nil}
   end
 
-  def prepare_request_auth({method, url, :no_auth}, _credentials, params) do
+  def prepare_request_auth({:get, url, :no_auth}, _credentials, params) do
     query =
       params
-      |> if_nil("")
+      |> if_nil(Map.new)
       |> URI.encode_query
-    {method, "#{url}?#{query}", nil, nil}
+    case query do
+      "" -> {:get, url, nil, nil}
+      _ -> {:get, "#{url}?#{query}", nil, nil}
+    end
   end
 
   defp if_nil(value, new_value) do
@@ -41,17 +46,24 @@ defmodule ExTumblr.Request do
 
   @spec sign_request_with_oauth(atom, String.t, Credentials.t, Keyword.t) :: {authorization_headers, Keyword.t}
   defp sign_request_with_oauth(method, url, credentials, params) do
-    oauth_creds = OAuther.credentials(
-      consumer_key: credentials.consumer_key,
-      consumer_secret: credentials.consumer_secret,
-      token: credentials.token,
-      token_secret: credentials.token_secret
-    )
-    params_as_keyword = case params do
-                          nil -> []
-                          _ -> Keyword.new(params)
-                        end
-    OAuther.sign(to_string(method), url, params_as_keyword, oauth_creds)
+    to_oauth_creds = fn %Credentials{} = creds ->
+      OAuther.credentials(
+        consumer_key: creds.consumer_key,
+        consumer_secret: creds.consumer_secret,
+        token: creds.token,
+        token_secret: creds.token_secret
+      )
+    end
+
+    to_string_tag = fn {key, value} when is_atom(key) -> {to_string(key), value} end
+
+    to_keyword = fn list ->
+      list
+      |> if_nil(Keyword.new)
+      |> Enum.map(to_string_tag)
+    end
+
+    OAuther.sign(to_string(method), url, to_keyword.(params), to_oauth_creds.(credentials))
     |> OAuther.header
   end
 end
--- test/ex_tumblr/request_test.exs
+++ test/ex_tumblr/request_test.exs
@@ -1,24 +1,116 @@
 defmodule ExTumblr.RequestTest do
   use ShouldI, async: true
 
-  alias ExTumblr.Request
+  alias ExTumblr.{Request, Credentials}
 
-  having "No authentication required by the API" do
+  having "A base request not requiring any authentication" do
     setup context do
-       context
-       |> assign(base_request: {:method, "my_url", :no_auth})
-       |> assign(params: %{my_param: "my value", my_other_param: "my other value"})
+      context
+      |> assign(base_request: {:get, "my_url", :no_auth})
     end
 
-    should "prepare a request with a no headers and no body", context do
-      {_, _, body, headers} = Request.prepare_request_auth(context.base_request, nil, context.params)
-      assert body == nil
-      assert headers == nil
+    having "no extra parameters" do
+      should "not alter the http method", context do
+        {:get, _, _, _} = Request.prepare_request_auth(context.base_request, nil, nil)
+      end
+
+      should "not append a query to the url", context do
+        {_, url, _, _} = Request.prepare_request_auth(context.base_request, nil, nil)
+        assert url == "my_url"
+      end
     end
 
-    should "encode and format the parameters", context do
-      {_, url, _, _} = Request.prepare_request_auth(context.base_request, nil, context.params)
-      assert url == "my_url?my_other_param=my+other+value&my_param=my+value"
+    having "extra parameters" do
+      setup context do
+        context
+        |> assign(params: %{my_param: "my value", my_other_param: "my other value"})
+      end
+
+      should "not alter the http method", context do
+        {:get, _, _, _} = Request.prepare_request_auth(context.base_request, nil, context.params)
+      end
+
+      should "prepare a request with no headers and no body", context do
+        {_, _, body, headers} = Request.prepare_request_auth(context.base_request, nil, context.params)
+        assert body == nil
+        assert headers == nil
+      end
+
+      should "encode and format the parameters", context do
+        {_, url, _, _} = Request.prepare_request_auth(context.base_request, nil, context.params)
+        assert url == "my_url?my_other_param=my+other+value&my_param=my+value"
+      end
+    end
+  end
+
+  having "A base request requiring an API key" do
+    setup context do
+      context
+      |> assign(base_request: {:get, "my_url", :api_key_auth})
+      |> assign(credentials: %Credentials{consumer_key: "abc"})
+    end
+
+    having "no extra parameters" do
+      should "specify the api key within the query", context do
+        {_, url, _, _} = Request.prepare_request_auth(context.base_request, context.credentials, nil)
+        assert url == "my_url?api_key=abc"
+      end
+
+      should "not create a body or headers", context do
+        {_, _, nil, nil} = Request.prepare_request_auth(context.base_request, context.credentials, nil)
+      end
+    end
+
+    having "extra parameters" do
+      setup context do
+        context
+        |> assign(params: %{my_param: "my value", my_other_param: "my other value"})
+      end
+
+      should "specify the api key within the query among other params", context do
+        {_, url, _, _} = Request.prepare_request_auth(context.base_request, context.credentials, context.params)
+        assert url == "my_url?my_other_param=my+other+value&my_param=my+value&api_key=abc"
+      end
+
+      should "not create a body", context do
+        {_, _, nil, _} = Request.prepare_request_auth(context.base_request, context.credentials, context.params)
+      end
+    end
+  end
+
+  having "A base request requiring an OAuth authentication" do
+    setup context do
+      fake_oauth_credentials =
+        %Credentials{
+          consumer_key: "abcd",
+          consumer_secret: "efgh",
+          token: "ijkl",
+          token_secret: "mnop"
+        }
+
+      context
+      |> assign(base_request: {:method, "https://api.tumblr.com/", :oauth})
+      |> assign(params: %{my_param: "my value", my_other_param: "my other value"})
+      |> assign(credentials: fake_oauth_credentials)
+    end
+
+    should "not alter the method type", context do
+      {:method, _, _, _} = Request.prepare_request_auth(context.base_request, context.credentials, context.params)
+    end
+
+    should "not alter the url", context do
+      {_, "https://api.tumblr.com/", _, _} = Request.prepare_request_auth(context.base_request, context.credentials, context.params)
+    end
+
+    should "create a body with the shape {:form, body}", context do
+      {_, _, {:form, _body}, _} = Request.prepare_request_auth(context.base_request, context.credentials, context.params)
+    end
+
+    should "create valid authorization headers", context do
+      {_, _, _, headers} = Request.prepare_request_auth(context.base_request, context.credentials, context.params)
+      [{"Authorization", oauth_details}] = headers
+      # here we must use a regexp because the headers are seeded with a timestamp
+      assert String.match? oauth_details, ~r{OAuth oauth_signature=\".*\", oauth_consumer_key=\"abcd\", oauth_nonce=\".*\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\".*\", oauth_version=\"1.0.*\", oauth_token=\"ijkl\"}
     end
   end
 end
