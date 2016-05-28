17ef8b7a9694cb739d8c7a67375bdafc83c521e9
Sebastien Varlet <sebastien.varlet@gmail.com>
2016-05-28 19:27:25 +0100

    Define a HTTP client adapter behaviour

--- lib/ex_tumblr/http_client_adapter.ex
+++ lib/ex_tumblr/http_client_adapter.ex
@@ -1,10 +1,18 @@
+defmodule ExTumblr.HTTPClientBehaviour do
+  @callback request(method :: atom, url :: String.t , body :: String.t, headers :: Keyword.t) :: any
+end
+
 defmodule ExTumblr.HTTPoisonAdapter do
+  @behaviour ExTumblr.HTTPClientBehaviour
+
   def request(method, url, body, headers) do
     HTTPoison.request(method, url, body || "", headers || [])
   end
 end
 
 defmodule ExTumblr.HTTPSpyAdapter do
+  @behaviour ExTumblr.HTTPClientBehaviour
+
   def request(method, url, body, headers) do
     send self, {method, url, body, headers}
   end
