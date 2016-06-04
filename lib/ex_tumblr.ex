5f81eddcad8cdf75a4abbe187574b3405618df2b
Sebastien Varlet <sebastien.varlet@gmail.com>
2016-06-04 20:40:03 +0100

    Expose all function of the blog api

--- lib/ex_tumblr.ex
+++ lib/ex_tumblr.ex
@@ -6,13 +6,6 @@ defmodule ExTumblr do
   """
   alias ExTumblr.{Blog, Request, HTTPResponseParser}
 
-  def blog_info(blog_identifier, credentials, params) do
-    blog_identifier
-    |> Blog.create_info_request
-    |> Request.prepare_request_auth(credentials, params)
-    |> emit
-  end
-
   def blog_avatar(blog_identifier, size \\ 64) do
     blog_identifier
     |> Blog.create_avatar_request(size)
@@ -21,29 +14,28 @@ defmodule ExTumblr do
     |> process_response
   end
 
-  def blog_followers(blog_identifier, credentials, params) do
-    blog_identifier
-    |> Blog.create_followers_request
-    |> Request.prepare_request_auth(credentials, params)
-    |> emit
-    |> process_response
-  end
-
-  def blog_likes(blog_identifier, credentials, params) do
-    blog_identifier
-    |> Blog.create_likes_request
-    |> Request.prepare_request_auth(credentials, params)
-    |> emit
-    |> process_response
-  end
-
-  def blog_posts(blog_identifier, credentials, params) do
-    blog_identifier
-    |> Blog.create_posts_request
-    |> Request.prepare_request_auth(credentials, params)
-    |> emit
-    |> process_response
-  end
+  [{"info", &Blog.create_info_request/1},
+   {"followers", &Blog.create_followers_request/1},
+   {"likes", &Blog.create_likes_request/1},
+   {"posts", &Blog.create_posts_request/1},
+   {"queued", &Blog.create_queued_posts_request/1},
+   {"drafts", &Blog.create_drafts_request/1},
+   {"submissions", &Blog.create_drafts_request/1},
+   {"post", &Blog.create_post_request/1},
+   {"edit", &Blog.create_edit_request/1},
+   {"reblog", &Blog.create_reblog_request/1},
+   {"delete", &Blog.create_delete_request/1}
+  ]
+  |> Enum.each(
+       fn {name, init_fun} ->
+         def unquote(String.to_atom(name))(blog_identifier, credentials, params) do
+           blog_identifier
+           |> unquote(init_fun).()
+           |> Request.prepare_request_auth(credentials, params)
+           |> emit
+           |> process_response
+         end
+       end)
 
   defp emit({method, url, body, headers}) do
     adapter = Application.get_env :ex_tumblr, :http_client
