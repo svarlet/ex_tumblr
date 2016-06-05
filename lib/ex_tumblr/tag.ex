11abf473acf41a4eef58f0f8f2c4c27bebedf0ac
Sebastien Varlet <sebastien.varlet@gmail.com>
2016-06-05 20:56:12 +0100

    Fix issues with the Tag module

--- lib/ex_tumblr/tag.ex
+++ lib/ex_tumblr/tag.ex
@@ -1,7 +1,7 @@
 defmodule ExTumblr.Tag do
   @hostname "https://api.tumblr.com"
 
-  def tagged(credentials, params) do
-    {:get, "#{@hostname}/v2/tagged", params, :api_key_auth, credentials}
+  def tagged do
+    {:get, "#{@hostname}/v2/tagged", :api_key_auth}
   end
 end
--- test/ex_tumblr_test.exs
+++ test/ex_tumblr_test.exs
@@ -5,7 +5,7 @@ defmodule ExTumblrTest do
 
   having "specified a custom HTTP client adapter" do
     should "send the prepared http request via the custom adapter" do
-      ExTumblr.blog_info "gunkatana.tumblr.com", %Credentials{consumer_key: "lol"}, nil
+      ExTumblr.info "gunkatana.tumblr.com", %Credentials{consumer_key: "lol"}, nil
       assert_received {:get, "https://api.tumblr.com/v2/blog/gunkatana.tumblr.com/info?api_key=lol", nil, nil}
     end
   end
