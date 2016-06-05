1e60b61500d5aa5be358e395e12ef372a90ecb09
Sebastien Varlet <sebastien.varlet@gmail.com>
2016-06-05 12:34:21 +0100

    Fix the test case of the Tag module

--- lib/ex_tumblr/tag.ex
+++ lib/ex_tumblr/tag.ex
@@ -1,4 +1,6 @@
 defmodule ExTumblr.Tag do
+  @hostname "https://api.tumblr.com"
+
   def tagged(credentials, params) do
     {:get, "#{@hostname}/v2/tagged", params, :api_key_auth, credentials}
   end
