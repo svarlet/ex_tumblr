169f1741a07a2b02abff027f52f7d01580eea71e
Sebastien Varlet <sebastien.varlet@gmail.com>
2016-06-08 13:38:47 +0100

    Convert the ex_tumblr_test tests to shouldi

--- test/ex_tumblr_test.exs
+++ test/ex_tumblr_test.exs
@@ -1,20 +1,25 @@
 defmodule ExTumblrTest do
-  use ExUnit.Case, async: true
+  use ShouldI, async: true
 
-  setup do
-    bypass = Bypass.open
-    Application.put_env(:ex_tumblr, :hostname, "http://localhost:#{bypass.port}")
-    {:ok, bypass: bypass}
-  end
-
-  test "handles the special status code returned for avatar requests", %{bypass: bypass} do
-    prebaked_response = "avatar_url=https://67.media.tumblr.com/avatar_34abdab07c47_48.png"
-    Bypass.expect bypass, fn conn ->
-      assert "/v2/blog/gunkatana.tumblr.com/avatar/48" == conn.request_path
-      conn
-      |> Plug.Conn.put_resp_header("status", "301 Moved Permanently")
-      |> Plug.Conn.resp(200, prebaked_response)
+  having "a running remote api" do
+    setup context do
+      bypass = Bypass.open
+      Application.put_env(:ex_tumblr, :hostname, "http://localhost:#{bypass.port}")
+      assign context, bypass: bypass
     end
-    {:ok, response} = ExTumblr.avatar "gunkatana.tumblr.com", 48
+
+    should "handle the particular response of the avatar endpoint", context do
+      prebaked_response = "avatar_url=https://67.media.tumblr.com/avatar_34abdab07c47_48.png"
+      Bypass.expect context.bypass, fn conn ->
+        assert "/v2/blog/gunkatana.tumblr.com/avatar/48" == conn.request_path
+        conn
+        |> Plug.Conn.put_resp_header("status", "301 Moved Permanently")
+        |> Plug.Conn.resp(200, prebaked_response)
+      end
+      {:ok, %HTTPoison.Response{body: body}} = ExTumblr.avatar "gunkatana.tumblr.com", 48
+      assert body == prebaked_response
+    end
+
+
   end
 end
