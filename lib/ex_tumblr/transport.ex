e35bddec5b2a701530af63897d434448d5a4d977
Sebastien Varlet <sebastien.varlet@gmail.com>
2016-06-18 18:08:15 +0100

    Add body and headers to the request logs

--- lib/ex_tumblr/transport.ex
+++ lib/ex_tumblr/transport.ex
@@ -4,7 +4,11 @@ defmodule ExTumblr.Transport do
       require Logger
 
       defp emit({method, url, body, headers}) do
-        Logger.info "#{__MODULE__}: Sending #{method} request to #{url}"
+        Logger.info """
+        #{__MODULE__}: Sending #{method} request to #{url} with
+          headers: #{inspect headers}
+          body: #{inspect body}
+        """
         HTTPoison.request(method, url, body || "", headers || [])
       end
     end
