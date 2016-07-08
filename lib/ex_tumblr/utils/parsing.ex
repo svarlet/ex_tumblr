b0c5dac494d16a814b18627a73b4aade04f55c91
Sébastien Varlet <sebastien.varlet@gmail.com>
2016-07-08 18:08:47 +0100

    Document the to_struct function.

--- lib/ex_tumblr/utils/parsing.ex
+++ lib/ex_tumblr/utils/parsing.ex
@@ -1,4 +1,15 @@
 defmodule ExTumblr.Utils.Parsing do
+  @doc """
+  Convert a map to a struct.
+
+  It expects the keys of the map to be strings. When the key does not
+  already exist as an atom, it is dropped. The reason behind this choice is
+  because the JSON responses may contain private or deprecated keys. They
+  would have to be transformed to atoms. Since they are not under our control
+  and because the number of created atoms should be controlled, we consider
+  that a string key which does not already have its matching atom is
+  irrelevant.
+  """
   def to_struct(nil, module) do
     struct module
   end
