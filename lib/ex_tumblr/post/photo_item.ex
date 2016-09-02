f631a013ce63bb219345063dd81bde1fa1e37c09
Sébastien Varlet <sebastien.varlet@gmail.com>
2016-09-02 18:15:38 +0100

    Define a property of the PhotoItem.parse function

--- lib/ex_tumblr/post/photo_item.ex
+++ lib/ex_tumblr/post/photo_item.ex
@@ -10,40 +10,12 @@ defmodule ExTumblr.Post.PhotoItem do
   defstruct ~w(caption original_size alt_sizes)a
 
   @doc """
-  Creates a struct from a `String.t` to `any` map.
+  Creates a struct from a map.
 
-  It reads the values from the `"caption"` and `"alt_sizes"` properties.
+  The keys of the map should be of type `String.t`. This function parses the `caption`,
+  `original_size` and `alt_sizes` properties.
   It expects the latter to be a list and will transform each item to a
   `PhotoMeta` struct.
-
-      iex> original_size = %{"width" => 1200, "height" => 1600, "url" => "http://a.url.com/pic.jpg"}
-      iex> alt_sizes = [
-      ...>   %{"width" => 600, "height" => 800, "url" => "http://a.url.com/pic1.jpg"},
-      ...>   %{"width" => 300, "height" => 400, "url" => "http://a.url.com/pic2.jpg"}
-      ...> ]
-      iex> map = %{"caption" => "Las Vegas 2014", "original_size" => original_size, "alt_sizes" => alt_sizes}
-      iex> ExTumblr.Post.PhotoItem.parse(map)
-      %ExTumblr.Post.PhotoItem{
-        caption: "Las Vegas 2014",
-        original_size:
-          %ExTumblr.Post.PhotoMeta{
-            width: 1200,
-            height: 1600,
-            url: "http://a.url.com/pic.jpg"
-          },
-        alt_sizes: [
-          %ExTumblr.Post.PhotoMeta{
-            width: 600,
-            height: 800,
-            url: "http://a.url.com/pic1.jpg"
-          },
-          %ExTumblr.Post.PhotoMeta{
-            width: 300,
-            height: 400,
-            url: "http://a.url.com/pic2.jpg"
-          }
-        ]
-      }
   """
   @spec parse(map) :: t
   def parse(map) when is_map(map) do
--- test/ex_tumblr/post/photo_item_test.exs
+++ test/ex_tumblr/post/photo_item_test.exs
@@ -1,4 +1,42 @@
 defmodule ExTumblr.Post.PhotoItemTest do
   use ExUnit.Case, async: true
-  doctest ExTumblr.Post.PhotoItem
+  use PropCheck
+
+  alias ExTumblr.Post.PhotoItem
+
+  defp url_gen, do: elements ["url1", "url2"]
+
+  defp photo_meta_gen do
+    let [width, height, url] <- [nat, nat, url_gen] do
+      %{"width" => width,
+        "height" => height,
+        "url" => url}
+    end
+  end
+
+  defp caption_gen, do: elements ["", "caption"]
+
+  defp photo_item_gen do
+    let [caption, original_size, alt_sizes] <- [caption_gen, photo_meta_gen, list(photo_meta_gen)] do
+      %{"caption" => caption,
+        "original_size" => original_size,
+        "alt_sizes" => alt_sizes}
+    end
+  end
+
+  property "all properties are parsed" do
+    forall raw_photo_item <- photo_item_gen do
+      photo_item =
+        raw_photo_item
+        |> PhotoItem.parse
+
+      # if any property is not/badly parsed, we will find out by reencoding
+      # to JSON, decoding and comparing to the generated value.
+      raw_photo_item ==
+        photo_item
+        |> Poison.encode!
+        |> Poison.decode!
+    end
+  end
+
 end
