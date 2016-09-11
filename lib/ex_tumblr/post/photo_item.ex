92ec34a725da1e90292a8ffd4cb51de359c8b5c4
Sebastien Varlet <sebastien.varlet@gmail.com>
2016-09-11 20:43:08 +0100

    Delete a superficial comment.

--- lib/ex_tumblr/post/photo_item.ex
+++ lib/ex_tumblr/post/photo_item.ex
@@ -9,14 +9,6 @@ defmodule ExTumblr.Post.PhotoItem do
 
   defstruct ~w(caption original_size alt_sizes)a
 
-  @doc """
-  Creates a struct from a map.
-
-  The keys of the map should be of type `String.t`. This function parses the `caption`,
-  `original_size` and `alt_sizes` properties.
-  It expects the latter to be a list and will transform each item to a
-  `PhotoMeta` struct.
-  """
   @spec parse(map) :: t
   def parse(map) when is_map(map) do
     %__MODULE__{
